895afa783e2611c532b07330e0377807a87df8e4
BMandruxa <riki-chiki5@yandex.ru>
2022-03-26 11:36:07 +0300

    com9

--- src/js/files/burger.js
+++ src/js/files/burger.js
@@ -2,23 +2,29 @@
 // Desktop
 const iconMenu = document.querySelector('.header__icon');
 const menuBody = document.querySelector('.header__menu');
+const block = document.querySelector('.display-block');
+const opacity = document.querySelector('.opacity-block');
 if (iconMenu) {
     iconMenu.addEventListener("click", () => {
-        document.body.classList.toggle('_lock');
+        // document.body.classList.toggle('_lock');
         iconMenu.classList.toggle('_active');
         menuBody.classList.toggle('_active');
+        block.classList.toggle('_display');
+        opacity.classList.toggle('_opacity')
     });
 }
 
-// Mobile
+// Mobile wrapper
 const iconMobile = document.querySelector('.mobile-body__icon');
 const menuMobile = document.querySelector('.header__content-mobile');
+const wrapper = document.querySelector('.wrapper-background');
 // const header = document.querySelector('.header');
 if (iconMobile) {
     iconMobile.addEventListener("click", () => {
-        // document.body.classList.toggle('_lock');
+        document.body.classList.toggle('_lock');
         iconMobile.classList.toggle('_mobile');
         menuMobile.classList.toggle('_mobile');
+        wrapper.classList.toggle('_background');
         // header.classList.toggle('_scroll')
     });
 }
